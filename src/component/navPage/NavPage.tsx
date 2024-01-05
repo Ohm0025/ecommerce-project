7d5adeb5c0aaceac4271462650a93c5336ecb02e
Ohm0025 <toyohm0025@gmail.com>
2024-01-05 22:41:23 +0700

    config nav page

--- src/component/navPage/NavPage.tsx
+++ src/component/navPage/NavPage.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams, useSearchParams } from "react-router-dom";
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import { formatPath } from "../../utils/formatText";
 import { getPathArr } from "../../utils/getPathArr";
+import { useLinkRef } from "../../store/linkRef";
 
 type Props = {};
 
@@ -12,34 +18,14 @@ const NavPage = (props: Props) => {
   const [isHome, setIsHome] = useState(false);
   const [arrPath, setArrPath] = useState<string[]>([]);
 
-  const urlParams = new URLSearchParams(window.location.search);
-  const search = urlParams.get("q");
-
-  useEffect(() => {
-    const initPath = location.pathname.split("/");
-
-    if (initPath.includes("products")) {
-      //console.log(formatPath("products"));
-      setArrPath((prev) => ["Home", "Products"]);
-      if (params.cat) {
-        setArrPath((prev) => {
-          return [...prev, params.cat];
-        });
-      }
-      if (search) {
-        setArrPath((prev) => {
-          return [...prev, search];
-        });
-      }
-      //setArrPath(() => getPathArr(formatPath("products")));
-    }
-  }, [location]);
+  const navigate = useNavigate();
+  const { query, brand, category } = useLinkRef();
 
   return (
     <div className="hidden sm:flex absolute top-[100%] left-[50%] w-full translate-x-[-50%] translate-y-[0] px-4 py-2 text-gray-400 bg-[#f7fafc] border border-gray-200">
       <div className="w-[80%] mx-auto">
         <ul className="flex">
-          {arrPath.map((item, index) => {
+          {/* {arrPath.map((item, index) => {
             return (
               item && (
                 <React.Fragment key={`path-item-${index}`}>
@@ -50,7 +36,45 @@ const NavPage = (props: Props) => {
                 </React.Fragment>
               )
             );
-          })}
+          })} */}
+          <li className="cursor-pointer" onClick={() => navigate("/")}>
+            home
+          </li>
+          <span className="mx-[10px]">{">"}</span>
+          <li className="cursor-pointer" onClick={() => navigate("/products")}>
+            products
+          </li>
+
+          {category && (
+            <>
+              <span className="mx-[10px]">{">"}</span>
+              <li
+                className="cursor-pointer"
+                onClick={() => navigate("/products/category/" + category)}>
+                {category}
+              </li>
+            </>
+          )}
+          {brand && (
+            <>
+              <span className="mx-[10px]">{">"}</span>
+              <li
+                className="cursor-pointer"
+                onClick={() => navigate("/products")}>
+                {brand}
+              </li>
+            </>
+          )}
+          {query && (
+            <>
+              <span className="mx-[10px]">{">"}</span>
+              <li
+                className="cursor-pointer"
+                onClick={() => navigate("/products/search/" + query)}>
+                {query}
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </div>
